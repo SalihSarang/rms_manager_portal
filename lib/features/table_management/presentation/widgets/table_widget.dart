97b2ce891d60a37136c52aedc2e701425341d2e6
Salih Sarang K <sarangsalih@gmail.com>
2026-03-19 15:05:15 +0530

    Refactor TableWidget logic into utils

--- lib/features/table_management/presentation/widgets/table_widget.dart
+++ lib/features/table_management/presentation/widgets/table_widget.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:rms_design_system/rms_design_system.dart';
 import 'package:rms_shared_package/rms_shared_package.dart';
+import 'table_widget/utils/table_widget_utils.dart';
 
 class TableWidget extends StatelessWidget {
   final TableModel table;
@@ -20,7 +21,11 @@ class TableWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final bool isCircle = table.shape == TableShape.circle;
     final (Color fill, Color border, Color textColor, Color badgeBg) =
-        _resolveColors();
+        TableWidgetUtils.resolveColors(
+      table: table,
+      isSelected: isSelected,
+      isPreview: isPreview,
+    );
 
     return Container(
       width: table.width,
@@ -47,7 +52,12 @@ class TableWidget extends StatelessWidget {
                       offset: const Offset(0, 3),
                     ),
                   ],
-        gradient: isPreview ? null : _resolveGradient(),
+        gradient: isPreview
+            ? null
+            : TableWidgetUtils.resolveGradient(
+                table: table,
+                isSelected: isSelected,
+              ),
       ),
       child: Center(
         child: Column(
@@ -101,91 +111,5 @@ class TableWidget extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  (Color, Color, Color, Color) _resolveColors() {
-    if (isPreview) {
-      return (
-        NeutralColors.surface,
-        NeutralColors.border,
-        NeutralColors.icon,
-        NeutralColors.background,
-      );
-    }
-    if (isSelected) {
-      return (
-        PrimaryColors.defaultColor,
-        PrimaryColors.hoverColor,
-        NeutralColors.white,
-        NeutralColors.white.withValues(alpha: 0.2),
-      );
-    }
-    switch (table.status) {
-      case TableStatus.occupied:
-        return (
-          TableColors.occupiedFill,
-          SemanticColors.error,
-          TableColors.occupiedText,
-          SemanticColors.error.withValues(alpha: 0.2),
-        );
-      case TableStatus.reserved:
-        return (
-          TableColors.reservedFill,
-          SemanticColors.info,
-          TableColors.reservedText,
-          SemanticColors.info.withValues(alpha: 0.2),
-        );
-      case TableStatus.billRequested:
-        return (
-          TableColors.billRequestedFill,
-          SemanticColors.warning,
-          TableColors.billRequestedText,
-          SemanticColors.warning.withValues(alpha: 0.2),
-        );
-      case TableStatus.cleaning:
-        return (
-          TableColors.cleaningFill,
-          SemanticColors.success,
-          TableColors.cleaningText,
-          SemanticColors.success.withValues(alpha: 0.2),
-        );
-      default:
-        return (
-          NeutralColors.card,
-          NeutralColors.border,
-          NeutralColors.white,
-          NeutralColors.border.withValues(alpha: 0.5),
-        );
-    }
-  }
-
-  LinearGradient? _resolveGradient() {
-    if (isSelected) return null;
-    switch (table.status) {
-      case TableStatus.occupied:
-        return const LinearGradient(
-          colors: [TableColors.occupiedGradientStart, TableColors.occupiedFill],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        );
-      case TableStatus.reserved:
-        return const LinearGradient(
-          colors: [TableColors.reservedGradientStart, TableColors.reservedFill],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        );
-      case TableStatus.billRequested:
-        return const LinearGradient(
-          colors: [TableColors.billRequestedGradientStart, TableColors.billRequestedFill],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        );
-      default:
-        return const LinearGradient(
-          colors: [NeutralColors.cardGradientStart, NeutralColors.cardGradientEnd],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        );
-    }
   }
 }
